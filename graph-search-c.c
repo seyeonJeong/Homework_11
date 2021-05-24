42c4f51e41ad589db90e50f4e14ccec9b6de4cbf
seyeonJeong <wjdtpdus828@naver.com>
2021-05-24 23:22:16 +0900

    commit 3

--- graph-search-c.c
+++ graph-search-c.c
@@ -164,4 +164,67 @@ void InsertVertex(GraphType* h, int key)
     h->vn = (h->vn) + 1; //정점의 번호에 1을 더해준다
 }
 
+void InsertEdge(GraphType* h, int edgekey1, int edgekey2)
+{
+    //필요한 변수 정의
+    short int a = 0;
+    int v1, v2 = 0;
+    GraphNode* node1, * node2;
+
+
+    for (int i = 0; i < h->vn; i++)
+    {
+        if (h->adj_list[i].vertex == edgekey1) //edgekey1와 같은 값을 가지는 정점이 존재하면 a에 1을 더해준다
+        {
+            a++;
+        }
+        if (h->adj_list[i].vertex == edgekey2) //edgekey2와 같은 값을 가지는 정점이 존재하면 a에 1을 더해준다
+        {
+            a++;
+        }
+    }
+    if (a != 2) {
+        //정점이 없습니다.
+        return;
+    }
+    //FindVertex함수를 이용하여 v1과 v2에 해당 인덱스 번호를 받아옴
+    v1 = FindVertex(h, edgekey1);
+    v2 = FindVertex(h, edgekey2);
+    //node1과 node2에 동적할당을 해준 후 vertex값을 각각 edgekey1,edgekey2로 설정해줌
+    node1 = (GraphNode*)malloc(sizeof(GraphNode));
+    node1->link = NULL;
+    node1->vertex = edgekey1;
+    node2 = (GraphNode*)malloc(sizeof(GraphNode));
+    node2->link = NULL;
+    node2->vertex = edgekey2;
+    //값이 설정된 node2와 node1을 이용하여 정렬을 해주면서 간선을 연결해준다.
+    sort(node2, h, v1);
+    sort(node1, h, v2);
+
+    return;
+}
+void DFS_Graph(GraphType* h, int u)
+{
+    int u1 = 0;
+    u1 = FindVertex(h, u); // u1에 해당 vertex값에 해당하는 인덱스값을 받아줌
+    int u2 = 0;
+
+    GraphNode* cur = NULL;
+    GraphNode* cur1 = NULL;
+    cur = h->adj_list; //cur은 h->adj_list배열로 설정
+    cur1 = cur[u1].link; // cur1을 해당 vertex값 즉, 사용자가 선택한 첫번째 탐색의 대상의 다음 link값을 설정
+    //탐색 시작
+    visited[u1] = TRUE; //첫번째 인덱스값에 해당하는 visited배열의 값을 TRUE로 설정한다.
+    printf("%d", cur[u1].vertex); //u1의 인덱스값에 해당하는 vertex값을 출력
+    printf("\n");
+
+    for (cur1; cur1; cur1 = cur1->link) //cur[u1]의 link에 해당하는 노드부터 NULL값이 될때까지 link를 반복이동하며 탐색
+    {
+        u2 = FindVertex(h, cur1->vertex); //u2에 cur1의 vertex에 해당하는 인덱스값을 설정
+        if (visited[u2] != TRUE) //만약 방문한 노드가 아니라면
+            DFS_Graph(h, cur[u2].vertex); //재귀를 이용하여 해당노드에 대한 탐색을 실행
+    }
+
+
+}
 
